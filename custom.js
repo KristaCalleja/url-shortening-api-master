75cb543a7f1282a948dced6e483184e619459690
KristaCalleja <kristacm@gmail.com>
2021-06-02 15:12:53 -0300

    Generate response div on submit

--- custom.js
+++ custom.js
@@ -1,3 +1,4 @@
+// Toggle mobile menu
 const navShow = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links-float');
@@ -6,5 +7,17 @@ const navShow = () => {
         nav.classList.toggle('nav-active');
     });
 }
+// Prompt on input focus
+const inputPrompt = () => {
+    const searchInput = document.getElementById('search-input');
+    const promptElement = document.getElementById('prompt');
 
+    searchInput.addEventListener('focus', (event) => {
+        promptElement.classList.toggle();
+        event.target.style.outline = 'red';
+    });
+}
+
+// Invoke all functions
 navShow();
+inputPrompt();
