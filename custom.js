469d5e3f60474d8ffdab80833295761d15231148
KristaCalleja <kristacm@gmail.com>
2021-07-06 18:11:50 -0300

    Add innerText

--- custom.js
+++ custom.js
@@ -3,6 +3,7 @@ const navFloat = document.querySelector('.nav-links-float');
 const searchInput = document.getElementById('search-input');
 const promptElement = document.querySelector('.prompt');
 const submitBtn = document.getElementById('btn');
+const midSection = document.querySelector('.mid-section');
 
 // Toggle mobile menu
 function navShow() {
@@ -19,7 +20,6 @@ searchInput.addEventListener('focus', inputPrompt);
 
 // CreateHTML on click
 function createCardDiv(){
-    const midSection = document.querySelector('.mid-section');
     const newCard = document.createElement('div');
     const inputUrl = document.createElement('div');
     const responseDiv = document.createElement('div');
@@ -28,38 +28,32 @@ function createCardDiv(){
     inputUrl.classList.add('input-url');
     responseDiv.classList.add('response');
     copyBtn.classList.add('form-btns');
-    inputUrl.textContent = 'Loading...';
-    responseDiv.textContent = 'Loading...';
+    inputUrl.innerText = 'Loading...';
     copyBtn.innerText = 'Copy!';
     midSection.insertAdjacentElement('beforebegin', newCard);
     newCard.insertAdjacentElement('afterbegin', inputUrl);
     newCard.insertAdjacentElement('afterbegin', responseDiv);
     newCard.insertAdjacentElement('beforeend', copyBtn);
+    responseDiv.innerText = searchInput.value;    
 }
 submitBtn.addEventListener('click', function(event){
     event.preventDefault();
     createCardDiv();
+    console.log(searchInput.value);
 });
+
+// Consume API and parse data
+
+    // const response = fetch(`https://api.shrtco.de/v2/shorten?url=${searchedUrl}`);
+    // const data = response.json();
+    
+    // console.log(data);
+    // console.log(data.ok);
+    // console.log(data.error);
+// }
 
 // Error handler
 function handleError(err){
     console.log("Error");
     console.log(err);
 }
-// function displayShorterUrl(url){
-//     const searchValue = document.getElementById('search-input');
-//     const searchedUrl = searchValue.value;
-//     console.log(searchedUrl);
-    
-
-    
-//     const response = fetch(`https://api.shrtco.de/v2/shorten?url=${searchedUrl}`);
-//     const data = response.json();
-    
-//     console.log(data);
-//     console.log(data.ok);
-//     console.log(data.error);
-//     inputtedUrl.textContent = `${data.ok}`;
-//     responseCode.textContent = `${data.error}`;
-// }
-// displayShorterUrl("https://frontendmentor.io");
