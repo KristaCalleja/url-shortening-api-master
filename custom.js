b369493ece8951cd3963ea2bc69b6654e38cd404
KristaCalleja <kristacm@gmail.com>
2021-07-07 10:59:31 -0300

    AJAX

--- custom.js
+++ custom.js
@@ -18,36 +18,47 @@ function inputPrompt() {
 }
 searchInput.addEventListener('focus', inputPrompt);
 
-// Consume API and parse data
-function consumeAPI(){
-    const searchedUrl = searchInput.value;
-    const data = fetch(`https://api.shrtco.de/v2/shorten?url=${searchedUrl}`);
-    console.log(typeof data);
-    console.log(data.ok);
-}
-
-// CreateHTML on click
-function createCardDiv(){
+    
+// Consume API and create it dynamically on page
+function returnResponse(){
     const newCard = document.createElement('div');
     const inputUrl = document.createElement('div');
     const responseDiv = document.createElement('div');
     const copyBtn = document.createElement('button');
+
+    // Style new divs
     newCard.classList.add('card');
     inputUrl.classList.add('input-url');
     responseDiv.classList.add('response');
     copyBtn.classList.add('form-btns');
     inputUrl.innerText = 'Loading...';
     copyBtn.innerText = 'Copy!';
+
+    // Manipulate the DOM
     midSection.insertAdjacentElement('beforebegin', newCard);
     newCard.insertAdjacentElement('afterbegin', inputUrl);
     newCard.insertAdjacentElement('afterbegin', responseDiv);
     newCard.insertAdjacentElement('beforeend', copyBtn);
-    responseDiv.innerText = searchInput.value;    
+
+    // Consume the API
+    const searchedUrl = searchInput.value;
+    const dataPromise = fetch(`https://api.shrtco.de/v2/shorten?url=${searchedUrl}`);
+    console.log(typeof dataPromise);
+    dataPromise
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            console.log(data.ok);
+            responseDiv.innerText = `${data.result.original_link}`;
+            inputUrl.textContent = `${data.result.short_link}`;
+        })
+        .catch(handleError);
 }
 submitBtn.addEventListener('click', function(event){
     event.preventDefault();
-    createCardDiv();
-    consumeAPI();
+    returnResponse();
 });
 
 
